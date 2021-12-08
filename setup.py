ad4233d7625432f3efe60c6c4ca939a72c152bc5
Craig Hobbs <craigahobbs@gmail.com>
2021-12-08 09:23:29 -0800

    schema-markdown 1.1.10

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def main():
         description='Human-friendly schema definition language and schema validator',
         long_description=long_description,
         long_description_content_type='text/x-rst',
-        version='1.1.9',
+        version='1.1.10',
         author='Craig A. Hobbs',
         author_email='craigahobbs@gmail.com',
         keywords='schema validation json',
