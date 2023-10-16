19bd5cdd109e42c448967ee2527d025bd991137c
Craig Hobbs <craigahobbs@gmail.com>
2023-10-16 15:55:02 -0700

    cleanup

--- src/schema_markdown/parser.py
+++ src/schema_markdown/parser.py
@@ -1,5 +1,5 @@
 # Licensed under the MIT License
-# https://github.com/craigahobbs/schema_markdown/blob/main/LICENSE
+# https://github.com/craigahobbs/schema-markdown/blob/main/LICENSE
 
 """
 Schema Markdown parser
